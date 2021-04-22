d2a03bc57d47f5ee1f65a9f749092c1572d7da58
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-04-22 20:10:12 +0300

    321

--- js/script.js
+++ js/script.js
@@ -30,14 +30,14 @@ $(document).ready(function () {
 		return false;
 	});
 
-	$('.create__discription').click(function () {
-		var $text_copy = $(this);
-		var $temp = $("<input>");
-		$("body").append($temp);
-		$temp.val($text_copy.text()).select();
-		document.execCommand("copy");
-		$temp.remove();
-		$('.copy_link_mess').fadeIn(400);
-		setTimeout(function () { $('.copy_link_mess').fadeOut(400); }, 5000);
-	});
+	// $('.create__discription').click(function () {
+	// 	var $text_copy = $(this);
+	// 	var $temp = $("<input>");
+	// 	$("body").append($temp);
+	// 	$temp.val($text_copy.text()).select();
+	// 	document.execCommand("copy");
+	// 	$temp.remove();
+	// 	$('.copy_link_mess').fadeIn(400);
+	// 	setTimeout(function () { $('.copy_link_mess').fadeOut(400); }, 5000);
+	// });
 });
